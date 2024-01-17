c85d211d0c438e8cf74ffd4a57572e0c6296d991
Quentin Latour <quentinlatour@openroam-prg-gm-1-128-216.net.univ-paris-diderot.fr>
2024-01-17 19:00:24 +0100

    fix ex03

--- day01/ex03/ft_print_numbers.c
+++ day01/ex03/ft_print_numbers.c
@@ -6,12 +6,12 @@
 /*   By: qlatour <qlatour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 18:06:24 by qlatour           #+#    #+#             */
-/*   Updated: 2024/01/17 18:09:55 by qlatour          ###   ########.fr       */
+/*   Updated: 2024/01/17 18:58:20 by qlatour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
-void	 ft_print_numbers()
+void	ft_print_numbers(void)
 {
-	write(1,"0123456789", 10);
+	write(1 "0123456789", 10);
 }
